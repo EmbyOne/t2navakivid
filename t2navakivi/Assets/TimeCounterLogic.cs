279e0bca3172ad3127b788b02608e7fa5229f984
Argo <argo.kamenik@gmail.com>
2022-06-01 17:36:42 +0300

    this is an innit, innit?

---
+++ t2navakivi/Assets/TimeCounterLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounterLogic : MonoBehaviour
+{
+    DateTime current = Datetime.Now;
+
+    public Text timeUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
